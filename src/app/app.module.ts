67c01da37201f4cf645e61b627ed1df95ad7ef57
Simon András Péter <simonandraspeter@gmail.com>
2024-04-03 17:53:57 +0200

    nodes and modules testing and practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { TestComponent2Component } from './test-component2/test-component2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent,
+    TestComponent2Component
   ],
   imports: [
     BrowserModule,
